e839c3181859b832fe9133b33cde790474780458
Lee Knowlton <leeknowlton@gmail.com>
2025-04-06 00:24:02 +0800

    Add support for multiple ENS contract addresses

--- api/audit/alchemy.js
+++ api/audit/alchemy.js
@@ -22,12 +22,24 @@ export async function addressToEns(address) {
   try {
     console.log(`[alchemy] Getting ENS domains for address ${address}`);
 
-    const ensContractAddress = "0xD4416b13d2b3a9aBae7AcD5D6C2BbDBE25686401";
-    const nfts = await alchemy.nft.getNftsForOwner(address, {
-      contractAddresses: [ensContractAddress],
-    });
-    const ensDomains = nfts.ownedNfts.map((nft) => nft.name);
-    return ensDomains;
+    const ensContractAddresses = [
+      "0xD4416b13d2b3a9aBae7AcD5D6C2BbDBE25686401",
+      "0x57f1887a8BF19b14fC0dF6Fd9B2acc9Af147eA85",
+    ];
+
+    const nftsPromises = ensContractAddresses.map((contractAddress) =>
+      alchemy.nft.getNftsForOwner(address, {
+        contractAddresses: [contractAddress],
+      }),
+    );
+
+    const nftsResults = await Promise.all(nftsPromises);
+    const ensDomains = nftsResults.flatMap((result) =>
+      result.ownedNfts.map((nft) => nft.name),
+    );
+    return ensDomains.filter(
+      (domain) => domain !== undefined && domain !== null,
+    );
   } catch (error) {
     console.error(
       `[alchemy] Error getting ENS domains for address ${address}:`,
