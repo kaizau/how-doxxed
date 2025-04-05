153689e896c398dbea64bbccdfc9b369014de8c5
Kai Zau <kaizau@users.noreply.github.com>
2025-04-05 16:01:11 +0800

    Browser and IP fingerprinting

---
+++ site/_shared/browser.js
@@ -0,0 +1,28 @@
+import { getFingerprint, getFingerprintData } from "@thumbmarkjs/thumbmarkjs";
+
+export async function analyzeBrowser() {
+  try {
+    const fingerprint = await getFingerprint();
+    const data = await getFingerprintData();
+    data.fingerprint = fingerprint;
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error("Error analyzing browser:", error);
+  }
+}
+
+async function analyzeLocation() {
+  try {
+    const response = await fetch("https://ipapi.co/json/");
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error("Error fetching location:", error);
+  }
+}
+
+// TODO Remove
+analyzeBrowser();
+analyzeLocation();
